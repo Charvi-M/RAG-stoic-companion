f4b6b18ae82d96215afd1ad4023caf67c59fe41d
Charvi Mishra <09charvimishra@gmail.com>
2025-07-25 13:47:22 +0530

    ver1

--- static/app.js
+++ static/app.js
@@ -15,7 +15,7 @@ async function askStoic() {
     answerBox.textContent = "Contemplating, It may take a minute ...";
 
     try {
-        const response = await fetch("http://127.0.0.1:5000/ask", {
+        const response = await fetch("/ask", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ question })
