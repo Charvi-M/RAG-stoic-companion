5cf23344f74a43b2f48c51e193d09ae111e3f24f
Charvi Mishra <09charvimishra@gmail.com>
2025-07-25 11:46:27 +0530

    deploy test 3

--- app.py
+++ app.py
@@ -30,5 +30,4 @@ def ask_question():
 if __name__ == "__main__":
     
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host="0.0.0.0", port=port)
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=port)
